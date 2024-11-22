3fe014eeca4bdf394b535e2528ef3c55c1c50bae
GreeneKP <greene.kevin.8118@gmail.com>
2024-11-22 11:31:51 -0700

    Add files via upload

--- spoofer/app.py
+++ spoofer/app.py
@@ -16,6 +16,7 @@ from datetime import datetime, timedelta
 from io import StringIO
 import statistics as stats
 import scipy.stats as scistats
+import os
 
 
 uriBase                = "https://www.space-track.org"
@@ -76,47 +77,62 @@ hdrs2 = {"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/a
 
 
 
-if datetime.now()-st.session_state['Timestamp']>timedelta(minutes=30):
-        st.session_state['Timestamp'] = datetime.now()
+#if datetime.now()-st.session_state['Timestamp']>timedelta(minutes=30):
+#        st.session_state['Timestamp'] = datetime.now()
 
-#if local, use these, otherwise...
+##if local, use these, otherwise...
 #satcat_time = pd.read_csv("data/satcat.csv")['Timestamp'].iloc[0][:19]
 #date_format = '%Y-%m-%d %H:%M:%S'
 #new_time = datetime.strptime(satcat_time, date_format)
-
-new_time = st.session_state['Timestamp']
-
-timestamp_difference = datetime.now() - new_time
-if timestamp_difference > timedelta(hours=3):
-    satcat = pd.read_csv("https://celestrak.org/pub/satcat.csv")
-    satcat['Timestamp'] = datetime.now()
-    satcat.to_csv('data/satcat.csv',header=True,sep=',')
-else: satcat = pd.read_csv("data/satcat.csv")
-
+#
+##new_time = st.session_state['Timestamp']
+#
+#timestamp_difference = datetime.now() - new_time
+#if timestamp_difference > timedelta(hours=3):
+#    satcat = pd.read_csv("https://celestrak.org/pub/satcat.csv")
+#    satcat['Timestamp'] = datetime.now()
+#    satcat.to_csv('data/satcat.csv',header=True,sep=',')
+#else: satcat = pd.read_csv("data/satcat.csv")
+#
+#satcat = pd.read_csv("https://celestrak.org/pub/satcat.csv")
 
 #The following code parses a ton of satellites out of the database so the user can only 
 #select satellites with enough pertinent information to actually let the tool function properly...
 #It's worth noting that the satellite catalog is a complete catalogue of everything that has EVER
-#been in space to include every individual piece of debris, deorbited sats, sputnik, etc. so it must
-#be parsed thoroughly so that the only available satellites to select are those that could reasonably
-#benefit from a prediction... No need to predict Sputniks Orbit or maneuvers afterall!
+##been in space to include every individual piece of debris, deorbited sats, sputnik, etc. so it must
+##be parsed thoroughly so that the only available satellites to select are those that could reasonably
+##benefit from a prediction... No need to predict Sputniks Orbit or maneuvers afterall!
+#
+##Sats must be considered functional and on orbit
+#satcat = satcat[satcat['OPS_STATUS_CODE'] == "+"]
+#
+##Sats must not be labelled as debris or rocket bodies (neither of which maneuver!)
+#satcat = satcat[satcat['OBJECT_TYPE'] != "DEB"]
+#satcat = satcat[satcat['OBJECT_TYPE'] != "R/B"]
+#
+##Sats must have complete data, without random gaps across different observations
+#satcat = satcat[satcat['DATA_STATUS_CODE'] != "NEA"]
+#satcat = satcat[satcat['DATA_STATUS_CODE'] != "NCE"]
+#satcat = satcat[satcat['DATA_STATUS_CODE'] != "NIE"]
+#
+##Sats must be orbitting the Earth
+#satcat = satcat[satcat['ORBIT_CENTER'] == "EA"]
+#
+#satcat = satcat[satcat['PERIOD'] > 1240]
+#satcat = satcat[satcat['PERIOD'] < 1640]
+#
+#collist = ['NORAD_CAT_ID','OWNER','LAUNCH_DATE','INCLINATION','APOGEE','PERIGEE']
+#
+#satcat.index = satcat['OBJECT_NAME']
+#
+#for col in satcat.columns:
+#    if col not in collist:
+#        satcat = satcat.drop(columns=col)
+#
+#
 
-#Sats must be considered functional and on orbit
-satcat = satcat[satcat['OPS_STATUS_CODE'] == "+"]
 
-#Sats must not be labelled as debris or rocket bodies (neither of which maneuver!)
-satcat = satcat[satcat['OBJECT_TYPE'] != "DEB"]
-satcat = satcat[satcat['OBJECT_TYPE'] != "R/B"]
 
-#Sats must have complete data, without random gaps across different observations
-satcat = satcat[satcat['DATA_STATUS_CODE'] != "NEA"]
-satcat = satcat[satcat['DATA_STATUS_CODE'] != "NCE"]
-satcat = satcat[satcat['DATA_STATUS_CODE'] != "NIE"]
-
-#Sats must be orbitting the Earth
-satcat = satcat[satcat['ORBIT_CENTER'] == "EA"]
-
-satcat = satcat[satcat['Period'] < 15]
 
 
 #Title of the app
@@ -126,13 +142,19 @@ st.title("GEO Database Builder")
 #I wanted to ensure no user imput errors were possible so, as such both available search criteria,
 #Name and satcat number, are in drop-down menus. 
 sat_selection_form = st.form("SCC selector")
-number_search = sat_selection_form.selectbox('Select NORAD SATCAT Number of a specific Satellite you want in GEO: ',satcat['NORAD_CAT_ID'])
+number_search = sat_selection_form.text_input('Select NORAD SATCAT Number of a specific Satellite you want in GEO: ')
 selection_criteria = sat_selection_form.radio('Select Number of Additional Satellites to put in GEO: ', options=['0','10','20','30','40','50'])
+
+#st.header("Available NORAD SATCAT (NORAD CAT ID) Reference")
+#
+#st.write(satcat)
 
 
 #I've come to find that once interacted with, 'submission' is set to 'True',
 #hence, the rest of the file will be in an if statement... 
 submission = sat_selection_form.form_submit_button("Build Database")
+
+
 
 today = datetime.today()
 today=str(today)
@@ -140,6 +162,7 @@ today=today[:10]
 
 onelist = []
 
+#tenlist = [37810, 42814, 39476, 37749, 45920, is broken27718, 41589, 38991, 34111, 32487]
 tenlist = [37810, 42814, 39476, 37749, 45920, 27718, 41589, 38991, 34111, 32487]
 
 twentylist = [40732, 37810, 40364, 42814, 36101, 39476, 39498, 37749, 37933, 45920, 39216, 27718, 42741, 41589, 33278, 38991, 37602, 34111, 33055, 32487]
@@ -147,7 +170,7 @@ twentylist = [40732, 37810, 40364, 42814, 36101, 39476, 39498, 37749, 37933, 459
 thirtylist = [44334, 54048, 37810, 43700, 47306, 42814, 38245, 39034, 39476, 38098, 41552, 37749, 39460, 42951, 45920, 36744, 40940, 27718, 32252, 37834, 41589, 28446, 39688, 38991, 42432, 40874, 34111, 43039, 37816, 32487]
 
 fortylist = [32299, 40732, 36582, 37810, 39215, 40364, 41384, 42814, 43632, 36101, 43698, 39476, 41747, 39498, 40880, 37749, 41028, 37933, 32019, 45920, 38331, 39216, 41836, 27718, 35756, 42741, 28187, 41589, 28252, 33278, 37809, 38991, 49055, 37602, 38867, 34111, 37264, 33055, 36830, 32487]
-
+#died at 42815 due to remote disconnection
 fiftylist = [38652, 41310, 28946, 44479, 37810, 32294, 29526, 41186, 29270, 42814, 49333, 44035, 37393, 42698, 39476, 38740, 41793, 39508, 45026, 37749, 42815, 35696, 46112, 41903, 45920, 42917, 41034, 43611, 28899, 27718, 25924, 43633, 40882, 26580, 41589, 39127, 36516, 28702, 33373, 38991, 40733, 43562, 38749, 43175, 34111, 28945, 42950, 40875, 32794, 32487]
 
 satlist = []
@@ -165,95 +188,103 @@ if submission:
     elif selection_criteria == '50':
         satlist = fiftylist
     
-pickedscc = number_search
+    pickedscc = int(number_search)
 
-satlist = satlist.append(pickedscc)
+    satlist.append(pickedscc)
+
+    st.subheader(f'a Database of the following SCCs will be built:') 
+    st.write(satlist)
+    st.write(f'Database Building in progress... Please wait... Estimated wait from start time to completion: {len(satlist)*3.3} minutes')
+
+    for i in range(len(satlist)):
+
+            zr = get(f"https://www.space-track.org/basicspacedata/query/class/gp_history/NORAD_CAT_ID/{satlist[i]}/orderby/TLE_LINE1%20ASC/EPOCH/2000-01-01--{today}/format/tle",headers=hdrs2)
+            oneline = zr.text
+            tlesraw = oneline.split('\r\n1')
+
+            for i in range(len(tlesraw)):
+               tlesraw[i] = tlesraw[i].replace('\r\n','|')
+
+            for i in range(len(tlesraw)):
+                tlesraw[i] = tlesraw[i].replace(' ','|')
+                tlesraw[i] = tlesraw[i].split('|')
+
+            tle_df = pd.DataFrame(tlesraw)
+            tle_df['Output']=''
+
+            def get_daytime(index_num):
+                jday = tle_df.iloc[index_num][5]
+
+                jday = float(jday)
+
+                date, time = divmod(jday, 1.0)
+
+                date = str(int(date))
+                #
+                #
+                #Next convert the fractional day into seconds:
+                #
+
+                timedelta(days=time)
+                #and add the two together:
+                #
+                output = datetime.strptime(date, '%y%j') + timedelta(time)
+
+                return output.strftime("TLE,%b %d %Y %H:%M:%S.%f,1 ")
+
+            for i in range(len(tle_df[0])):
+                try:
+                    tle_df[0].iloc[i] = get_daytime(i)
+                except:
+                    tle_df[0].iloc[i] = 'Flag for Del'
+            
+
+            tle_df = tle_df[tle_df[0] != 'Flag for Del']
+            tle_df = tle_df.map(lambda x: str(x) if type(x) != type(str) else x)
+            tle_df = tle_df.map(lambda x: '' if x == 'None' else x)
+            tle_df = tle_df.map(lambda x: x + ' ' if type(x) == str and len(x)>1 else x)
+
+            for h in range(28):
+                for col in tle_df.columns[:-2]:
+                    for i in range(len(tle_df[col])):
+                        if tle_df[col].iloc[i] == '' and tle_df[col].iloc[i] != tle_df[col+1].iloc[i]:
+                            tle_df[col].iloc[i] = tle_df[col+1].iloc[i] 
+                            tle_df[col+1].iloc[i] = ''
+
+            for col in tle_df.columns[:-1]:
+                if tle_df[col].iloc[0] == tle_df[col].iloc[len(tle_df)-1] and tle_df[col].iloc[0] == '':
+                    tle_df = tle_df.drop(columns=col)
+
+            tle_df[3] = '  ' + tle_df[3]
+            tle_df[8] = ''
+            tle_df[9] = '   1,2'
+            tle_df[10] = ' ' + tle_df[10] + '  '
+            tle_df[14] = ' ' + tle_df[14] + ' '
+
+            tle_df['Output']=tle_df.sum(axis=1,skipna=True)
+            tle_df['Output'] = tle_df['Output'] + '\n'
+
+            tle_df['JDATE']=None
+            for i in range(len(tle_df)):
+                tle_df['JDATE'].iloc[i] = tle_df[3].iloc[i]
+                tle_df['JDATE'].iloc[i] = tle_df['JDATE'].iloc[i][2:7]
+            
+            tle_df = tle_df.sort_values('JDATE',ascending=False)
 
 
-for sat_num in satlist:
-    zr = get(f"https://www.space-track.org/basicspacedata/query/class/gp_history/NORAD_CAT_ID/{sat_num}/orderby/TLE_LINE1%20ASC/EPOCH/2000-01-01--{today}/format/tle",headers=hdrs2)
-    oneline = zr.text
-    tlesraw = oneline.split('\r\n1')
+            for i in range(len(tle_df)):
+                jate = tle_df['JDATE'].iloc[i]
+                filepath = f'DATABASE/StateProcessing/StateDatabase/{jate}_Database_States.txt'
 
-for i in range(len(tlesraw)):
-   tlesraw[i] = tlesraw[i].replace('\r\n','|')
-
-for i in range(len(tlesraw)):
-    tlesraw[i] = tlesraw[i].replace(' ','|')
-    tlesraw[i] = tlesraw[i].split('|')
-
-tle_df = pd.DataFrame(tlesraw)
-tle_df['Output']=''
-
-def get_daytime(index_num):
-    jday = tle_df.iloc[index_num][5]
-
-    jday = float(jday)
-
-    date, time = divmod(jday, 1.0)
-
-    date = str(int(date))
-    #
-    #
-    #Next convert the fractional day into seconds:
-    #
-
-    timedelta(days=time)
-    #and add the two together:
-    #
-    output = datetime.strptime(date, '%y%j') + timedelta(time)
-
-    return output.strftime("TLE,%b %d %Y %H:%M:%S.%f,1 ")
-
-for i in range(len(tle_df[0])):
-    tle_df[0].iloc[i] = get_daytime(i)
-
-tle_df = tle_df.map(lambda x: str(x) if type(x) != type(str) else x)
-tle_df = tle_df.map(lambda x: '' if x == 'None' else x)
-tle_df = tle_df.map(lambda x: x + ' ' if type(x) == str and len(x)>1 else x)
-
-for h in range(28):
-    for col in tle_df.columns[:-2]:
-        for i in range(len(tle_df[col])):
-            if tle_df[col].iloc[i] == '' and tle_df[col].iloc[i] != tle_df[col+1].iloc[i]:
-                tle_df[col].iloc[i] = tle_df[col+1].iloc[i] 
-                tle_df[col+1].iloc[i] = ''
-
-for col in tle_df.columns[:-1]:
-    if tle_df[col].iloc[0] == tle_df[col].iloc[len(tle_df)-1] and tle_df[col].iloc[0] == '':
-        tle_df = tle_df.drop(columns=col)
-
-tle_df[3] = '  ' + tle_df[3]
-tle_df[8] = ''
-tle_df[9] = '   1,2'
-tle_df[10] = ' ' + tle_df[10] + '  '
-tle_df[14] = ' ' + tle_df[14] + ' '
-
-tle_df['Output']=tle_df.sum(axis=1,skipna=True)
-tle_df['Output'] = tle_df['Output'] + '\n'
-
-tle_df['JDATE']=None
-for i in range(len(tle_df)):
-    tle_df['JDATE'].iloc[i] = tle_df[3].iloc[i]
-    tle_df['JDATE'].iloc[i] = tle_df['JDATE'].iloc[i][2:7]
-
-import os
+                if os.path.exists(filepath):
+                    with open(filepath, 'a') as file:
+                        file.write(tle_df['Output'].iloc[i])
+  #                      file.close
+                else:
+                    with open(filepath, 'w') as file:
+                        file.write(tle_df['Output'].iloc[i])
+   #                     file.close
 
 
-for i in range(len(tle_df)):
-    jate = tle_df['JDATE'].iloc[i]
-    filepath = f'DATABASE/StateProcessing/StateDatabase/{jate}_Database_States.txt'
-
-    if os.path.exists(filepath):
-        with open(filepath, 'a') as file:
-            file.write(tle_df['Output'].iloc[i])
-            file.close
-    else:
-        with open(filepath, 'w') as file:
-            file.write(tle_df['Output'].iloc[i])
-            file.close
-    
-
-for i in range(len(tle_df)):
-    tle_df[i] = tle_df[i].split(' ')
-
+        #    st.write('Database pull for SCC not functioning. Moving on to next.')
+    st.header('Database Successfully built!')
